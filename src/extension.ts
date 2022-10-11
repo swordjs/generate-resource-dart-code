f64d878f7adb5b3ac263eaf9ef0e6128e53963af
seho <1018715564@qq.com>
2022-10-11 18:31:08 +0800

    ✨ feat: finish assets.dart generate

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,126 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { posix } from 'path';
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+type AssetsFolder = 'images' | 'svgs';
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "generate-resource-dart-code" is now active!');
+// 资源缓存对象
+const assets: Record<AssetsFolder, Record<string, string>> = { images: {}, svgs: {} };
+// dart code缓存对象
+const dartCodeCache: Record<AssetsFolder, string> = { images: '', svgs: '' };
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('generate-resource-dart-code.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from generate-resource-dart-code!');
-	});
+// debounce
+const debounce = (fn: Function, delay: number) => {
+	let timer: NodeJS.Timeout;
+	return (...args: any[]) => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			fn(...args);
+		}, delay);
+	};
+};
 
-	context.subscriptions.push(disposable);
+// 处理file格式, 返回符合要求的文件名和文件格式
+const handleFile = (file: vscode.Uri): { fileNameCamel?: string; fileFormatCamel?: string } => {
+	// 从path中获取文件名, 排除格式
+	const fileName = file.path.split('/').pop()?.split('.').shift();
+	// 从文件名中获取格式
+	const fileFormatCamel = file.path
+		.split('/')
+		.pop()
+		?.split('.')
+		.pop()
+		?.replace(/^\S/, (s) => s.toUpperCase());
+	// 判断name是否是下划线命名或者-命名, 如果是, 就转换成驼峰命名
+	const fileNameCamel = fileName?.replace(/[-_]\S/g, (s) => s.slice(1).toUpperCase());
+	return {
+		fileNameCamel,
+		fileFormatCamel
+	};
+};
+
+// 处理缓存对象的数据结构, 提供file对象和缓存对象引用为参数, 返回缓存对象引用
+type CacheHandler = (file: vscode.Uri, cache: Record<string, string>) => Record<string, string>;
+const cacheHandler: CacheHandler = (file, cache) => {
+	const { fileNameCamel, fileFormatCamel } = handleFile(file);
+	// 把key value对添加到对象中
+	cache[`${fileNameCamel}${fileFormatCamel}`] = file.path.substring(file.path.indexOf('assets'));
+	return cache;
+};
+
+// 将缓存对象转换为dart code
+const cacheToDartCode = async (
+	cache: Record<string, string>,
+	className: AssetsFolder,
+	options?: {
+		// 是否需要覆盖assets.dart文件
+		cover?: boolean;
+	}
+): Promise<void> => {
+	const val = Object.entries(cache)
+		.map(([key, value]) => `  static const ${key} = '${value}';`)
+		.join('\n');
+	const dartCode = `class Assets${className?.replace(/^\S/, (s) => s.toUpperCase())} {\n${val}\n}`;
+	// 将code缓存到dartCodeCache中
+	dartCodeCache[className] = dartCode;
+	// 如果需要覆盖原有的assets.dart文件, 就写入文件
+	if (options?.cover) {
+		// 循环缓存的dart code
+		let code = '';
+		Object.entries(dartCodeCache).forEach(([key, value]) => {
+			code += value + '\n\n';
+		});
+		const folderUri = vscode.workspace.workspaceFolders![0].uri;
+		const fileUri = folderUri.with({ path: posix.join(folderUri.path, 'lib', 'assets.dart') });
+		await vscode.workspace.fs.writeFile(fileUri, Buffer.from(code));
+		// vscode成功提示
+		vscode.window.showInformationMessage(
+			'[generate-resource-dart-code] assets.dart文件已成功生成🎉'
+		);
+	}
+};
+
+export async function activate(context: vscode.ExtensionContext) {
+	// 获取当前项目的根目录第一级文件
+	const isFlutter = await vscode.workspace.findFiles('pubspec.yaml');
+	if (isFlutter.length === 0) {
+		// 如果不存在pubspec.yaml文件，直接返回
+		return;
+	}
+	let key: AssetsFolder;
+	for (key in assets) {
+		const watcher = vscode.workspace.createFileSystemWatcher(`**/assets/${key}/**`);
+		// 遍历对应资源文件夹下的所有文件
+		const files = await vscode.workspace.findFiles( `assets/${key}/**`);
+		// 初始化缓存对象
+		files.forEach((file) => {
+			cacheHandler(file, assets[key]);
+		});
+		cacheToDartCode(assets[key], key, { cover: true });
+		watcher.onDidCreate(
+			debounce((uri: vscode.Uri) => {
+				cacheHandler(uri, assets[key]);
+				cacheToDartCode(assets[key], key, { cover: true });
+			}, 1000)
+		);
+		watcher.onDidDelete(
+			debounce(async (uri: vscode.Uri) => {
+				const { fileNameCamel, fileFormatCamel } = handleFile(uri);
+				if (assets[key][`${fileNameCamel}${fileFormatCamel}`]) {
+					delete assets[key][`${fileNameCamel}${fileFormatCamel}`];
+				}
+				cacheToDartCode(assets[key], key, { cover: true });
+			}, 1000)
+		);
+		watcher.onDidChange(
+			debounce(async (uri: vscode.Uri) => {
+				const { fileNameCamel, fileFormatCamel } = handleFile(uri);
+				if (assets[key][`${fileNameCamel}${fileFormatCamel}`]) {
+					delete assets[key][`${fileNameCamel}${fileFormatCamel}`];
+				}
+				// 重新添加
+				cacheHandler(uri, assets[key]);
+				cacheToDartCode(assets[key], key, { cover: true });
+			}, 1000)
+		);
+	}
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
